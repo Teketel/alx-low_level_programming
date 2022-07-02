80eeece8e3d6f88662e17ae462b17d2c7600d71b
teketel <for.teketel@gmail.com>
2022-07-02 16:49:04 +0300

    Betty-style and doc fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
-int main() {
+
+/**
+ * main - Program entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
 	return (0);
 }
