553641d5c67ad125c2b225c20c17dfe9c8f1728a
teketel <for.teketel@gmail.com>
2022-07-02 16:21:14 +0300

    Betty style fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main() {
+
+/** 
+ * Main function 
+ * entry point 
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void) 
+{
 
 	char str[] = "\"Programming is like building a multilingual puzzle";
 	puts(str);
