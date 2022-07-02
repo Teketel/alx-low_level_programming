6266bb4b33d0b40e133663d4285eafbcfccde1d9
teketel <for.teketel@gmail.com>
2022-07-02 16:50:22 +0300

    Betty-style and doc fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
-int main() {
 
+/**
+ * main - Program entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
 	printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
 	printf("Size of a int: %d byte(s)\n", (int) sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
