92a88d62d6daab835a4eec14f73111cf0235f2e0
teketel <for.teketel@gmail.com>
2022-07-05 22:00:05 +0300

    Fix on task 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,8 @@
 #include "_putchar.h"
 /**
  * print_alphabet - print_alphabet block
- * Write a function that prints the alphabet, in lowercase, followed by a new line. 
+ * Write a function that prints the alphabet,
+ * in lowercase, followed by a new line.
  * Return: void
  */
 void print_alphabet(void)
@@ -9,9 +10,7 @@ void print_alphabet(void)
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
-	{
-		_putchar(c);	
-	}
+		_putchar(c);
 	_putchar(10);
 
 }
