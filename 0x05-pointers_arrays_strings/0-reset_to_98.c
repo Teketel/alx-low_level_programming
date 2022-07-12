322a6fd57cbbb8dcafec5ee6f25ac8b85330f9ba
teketel <for.teketel@gmail.com>
2022-07-12 12:07:05 +0300

    Done task 1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - reset the value on address `n`
+ * @n: int type param
+ *
+ * Return: void.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
