8118e191555186a78e769d87e1c904dfe0392316
teketel <for.teketel@gmail.com>
2022-07-13 22:09:15 +0300

    Done task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,8 +1,20 @@
-#include <srdio.h>
+#include <stdio.h>
 
 /**
  * leet - encode a string into 1337
  * @s: string
+ * Description: 
+ * Letters a and A should be replaced by 4
+ * Letters e and E should be replaced by 3
+ * Letters o and O should be replaced by 0
+ * Letters t and T should be replaced by 7
+ * Letters l and L should be replaced by 1
+ * Prototype: char *leet(char *);
+ * You can only use one if in your code
+ * You can only use two loops in your code
+ * You are not allowed to use switch
+ * You are not allowed to use any ternary operation
+ * 
  * Return: encoded string `s`
  */
 
@@ -12,16 +24,41 @@ char *leet(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		while (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		while (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		while (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		while (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		while (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		switch (s[i])
+		{
+			case 'a':
+				s[i] = '4';
+				break;
+			case 'A':
+				s[i] = '4';
+				break;
+			case 'e':
+				s[i] = '3';
+				break;
+			case 'E':
+				s[i] = '3';
+				break;
+			case 'o':
+				s[i] = '0';
+				break;
+			case 'O':
+				s[i] = '0';
+				break;
+			case 't':
+				s[i] = '7';
+				break;
+			case 'T':
+				s[i] = '7';
+				break;
+			case 'l':
+				s[i] = '1';
+				break;
+			case 'L':
+				s[i] = '1';
+				break;
+			default:
+				break;
+		}
 	}
 
 	return (s);
