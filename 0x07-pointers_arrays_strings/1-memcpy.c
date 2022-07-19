eccafd59673c7d153fedf0f3a780d936c57cce97
teketel <for.teketel@gmail.com>
2022-07-19 22:34:54 +0300

    Try to fix 0x07-pointers_arrays_strings/task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,19 +1,18 @@
 #include "main.h"
 
 /**
- * _memcpy - copies n bytes from memory area `src` to memory area `dest`
- * @src: string type
- * @dest: string type
- * @n: int type
- * Return: a pointer to dest
- **/
-
+ * _memcpy - copies memory area,
+ * @dest: destination memory area.
+ * @src: source memory area.
+ * @n: bytes filled.
+ * Return: the pointer to dest.
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < n; n++)
-		*(dest + i) = *(src + i);
+	for (i = 0; i < n; i++)
+		*(dest + i) =  *(src + i);
 
 	return (dest);
 }
