17b9b306426063d394b4498e15a01d1a3bc4d930
teketel <for.teketel@gmail.com>
2022-07-19 22:21:51 +0300

    Done 0x07-pointers_arrays_strings/ Fix on task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -6,7 +6,7 @@
  * @needle: string type
  * Return: a pointer to the beginning of the located substring, or
  * NULL if the substring is not found.
- **/ 
+ **/
 
 char *_strstr(char *haystack, char *needle)
 {
