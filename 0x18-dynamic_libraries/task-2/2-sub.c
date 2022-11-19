c500e71a83bc315c12e45517de632b40950352df
teketel <for.teketel@gmail.com>
2022-11-19 11:14:55 +0300

    Ådd task 2, src code

---
+++ 0x18-dynamic_libraries/task-2/2-sub.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - a function that subtract two integers and returns the result.
+ * @a: int type number
+ * @b: int type number
+ * Return: result of addition type int
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
